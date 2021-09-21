638a3ae16de0c605bdddfed41670b9ae1fb51701
Yosef-S-A <yosefsamuel22@gmail.com>
2021-09-21 21:19:12 +0000

    task 3

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+from models import storage
+from api.v1.views import app_views
+import os
+
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+if __name__ == "__main__":
+   app.run(host=host, port=port)
---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+from api.v1.views import app_views
+from flask import jsonify, request
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """
+    function for status route that returns the status
+    """
+    if request.method == 'GET':
+        resp = {"status": "OK"}
+        return jsonify(resp)
