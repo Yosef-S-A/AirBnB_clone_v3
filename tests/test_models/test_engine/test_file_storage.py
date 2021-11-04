79efe139608bedc425d3e50ca9f5d779baed1e54
Yosef-S-A <yosefsamuel22@gmail.com>
2021-11-04 06:17:29 +0000

    task

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -67,6 +67,7 @@ test_file_storage.py'])
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
+
 @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
